aac6990f473143f77496b5738f2aacd7e5d28840
Jonny Johannes <jonny@jonnyjohannes.com>
2023-01-13 00:18:11 -0500

    add brew install instructions

--- src/App.js
+++ src/App.js
@@ -25,15 +25,28 @@ function App() {
   return (
     <div className="AestheticClock">
       <header className="AestheticClock-header">
+        <h1>AestheticClock macOs Screensaver</h1>
         {`${getCalendarComponent('hours')} ${getCalendarComponent('minutes')} ${getCalendarComponent('seconds')}`}
-        <div class="footer-nudge">
-          <a href="#AestheticClock-body" class="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
+        <div className="footer-nudge">
+          <a href="#AestheticClock-body" className="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""> </a>
         </div>
       </header>
       <div id="AestheticClock-body">
-        <p>
-          AestheticClock macOS Screensaver: <a href={process.env.PUBLIC_URL + '/AestheticClockSaver.zip'} className='AestheticClock-link' download>download</a>
-        </p>
+        AestheticClock macOS Screensaver install:
+        <ul>
+          <li>
+            <a href={process.env.PUBLIC_URL + '/AestheticClockSaver.zip'} className='AestheticClock-link' download>download</a>
+          </li>
+          <li>
+            or via homebrew
+            <pre>
+              brew tap jonnyjohannes/aesthetic-clock-screensaver
+              <br/>
+              brew install aesthetic-clock-screensaver
+            </pre>
+          </li>
+        </ul>
+
       </div>
     </div>
   );
